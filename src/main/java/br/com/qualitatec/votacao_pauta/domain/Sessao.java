4dcce85e0d8cc8b901c7cd9ed2dd865cca5b2f45
Rodrigo Rocha <rodrigo.lyan@gmail.com>
2026-03-13 17:07:48 -0300

    Ajuste na entidade sessão

--- src/main/java/br/com/qualitatec/votacao_pauta/domain/Sessao.java
+++ src/main/java/br/com/qualitatec/votacao_pauta/domain/Sessao.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Sessao {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
